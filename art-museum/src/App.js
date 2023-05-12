ee69ca562b4e55421aa54ce9561955f626d5dd33
Arvid Hossain <arvidh97@gmail.com>
2023-05-12 16:47:28 -0400

    phase 3

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -2,15 +2,29 @@ import harvardArt from "./data/harvardArt";
 import GalleryNavigation from "./components/GalleryNavigation";
 import GalleryView from "./components/GalleryVIew";
 import { Route } from "react-router-dom";
+import { Redirect, Switch } from "react-router-dom/cjs/react-router-dom.min";
 // console.log(harvardArt);
 
 function App() {
   return (
     <div className="page-wrapper">
         <GalleryNavigation galleries={harvardArt.records}/>
+        <Switch>
+        {/* <Route path='/error'>
+            <h2>Page Not Found</h2>
+        </Route> */}
         <Route path="/galleries/:galleryId">
             <GalleryView galleries={harvardArt.records}/>
         </Route>
+        <Route path='/:error'>
+          <h2>Page Not Found</h2>
+        </Route>
+        <Route path="/">
+          <h2>Harvard Art Museum</h2>
+          <p>Look, but Don't Touch. Please select a Gallery in the navigation bar.</p>
+        </Route>
+        {/* <Redirect to='/error'/> */}
+        </Switch>
     </div>
   );
 }
--- art-museum/src/components/GalleryVIew/index.js
+++ art-museum/src/components/GalleryVIew/index.js
@@ -1,10 +1,15 @@
-import { useParams } from "react-router-dom";
+import { useParams, Redirect } from "react-router-dom";
 
 const GalleryView = props => {
     let { galleryId } = useParams();
     const {galleries} = props;
     // console.log(galleries)
     const gallery = galleries.find(el => el.galleryid === parseInt(galleryId));
+    if (!gallery) {
+        return (
+            <Redirect to='/not-found'></Redirect>
+        )
+    }
     // console.log(gallery)
     return (
         <div>
