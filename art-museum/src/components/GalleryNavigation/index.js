4414cdb25318bc22d710e976a6ce6ba345dbb227
Arvid Hossain <arvidh97@gmail.com>
2023-05-12 15:35:02 -0400

    added nav link

---
+++ art-museum/src/components/GalleryNavigation/index.js
@@ -0,0 +1,26 @@
+import {NavLink} from 'react-router-dom';
+
+const GalleryNavigation = props => {
+    const {galleries} = props;
+    const galleryList = galleries.map(gallery => {
+        return (
+            <li key={gallery.id}>
+            <NavLink to={`/galleries/${gallery.id}`}>
+                {gallery.name}
+            </NavLink>
+            </li>
+        )
+    })
+    // console.log(galleries)
+    return(
+        <nav>
+        <h1>Galleries</h1> 
+        <NavLink to='/'>Homepage</NavLink>
+        <ul>
+            {galleryList}   
+        </ul>
+        </nav>
+    )
+}
+
+export default GalleryNavigation;
